0725b385642fb954d35e7c9d8cf84782b426d9cf
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-03 09:05:11 +0700

    first commit

---
+++ models/buku.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    const buku = sequelize.define("buku", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        judul: {
+            type: DataTypes.STRING,
+        },
+        pengarang: {
+            type: DataTypes.STRING,
+        },
+        tahunterbit: {
+            type: DataTypes.INTEGER,
+        },
+        bidang: {
+            type: DataTypes.STRING,
+        }
+    });
+    return buku;
+}
